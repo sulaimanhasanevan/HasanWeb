5633c92e02b6d65c2764bb630c392e5533ce601a
Sulaiman hasan Evan <72876870+sulaimanhasanevan@users.noreply.github.com>
2025-08-28 02:30:13 +0600

    debug console updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react' // or vue, etc.
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js', // if you have a separate config
+  },
 })
